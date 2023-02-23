10db99dd2a52b8b90f540608c71da702f021b73c
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-02-23 22:08:34 +0600

    update

---
+++ CODEFORCE/1762A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        ll sum = 0, ans = 21;
+        vector<ll> a(n);
+        for (auto &it : a)
+        {
+            cin >> it;
+            sum += it;
+        }
+        if (sum & 1)
+        {
+            for (auto &it : a)
+            {
+                ll cur = it, now = 0;
+                while (!((cur + it) & 1))
+                {
+                    now++;
+                    cur /= 2;
+                }
+                ans = min(ans, now);
+            }
+        }
+        else
+        {
+            ans = 0;
+        }
+        cout << ans << "\n";
+    }
+}
