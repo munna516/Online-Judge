c08394f1d9b5c8bb1fd5eead14596eb0790bb879
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-03-03 19:11:32 +0600

    update

--- CODEFORCE/Templete.cpp
+++ CODEFORCE/Templete.cpp
@@ -1,18 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define ll long long
+#define pi 3.1416
+#define endl '\n'
 
+void solve()
+{
+}
 int main()
 {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
+  cout.tie(0);
   ll t;
-  cin>>t;
-  while(t--)
+  cin >> t;
+  while (t--)
   {
-    
+    solve();
   }
-
   return 0;
 }
