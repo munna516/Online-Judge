92966e81472816af54150c26eed4df85240555f4
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-03-10 21:08:43 +0600

    update

--- CODECHEF/TALLER.cpp
+++ CODECHEF/TALLER.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
- int t;
- std::cin>>t;
- while(t--)
-   {
-    int x,y;
-    std::cin>>x>>y;
-    if(x>y)
-      {
-        std::cout<<"A"<<std::endl;
-      }
-    else  
-      {
-        std::cout<<"B"<<std::endl;
-      }  
-   }
+  int t;
+  std::cin >> t;
+  while (t--)
+  {
+    int x, y;
+    std::cin >> x >> y;
+    if (x > y)
+    {
+      std::cout << "A" << std::endl;
+    }
+    else
+    {
+      std::cout << "B" << std::endl;
+    }
+  }
 
- return 0; 
+  return 0;
 }
--- CODECHEF/TFPAPER.cpp
+++ CODECHEF/TFPAPER.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
- int t,n,k;
- std::cin>>t;
- while(t--)
-   {
-    std::cin>>n>>k;
-    int sub=n-k;
-    std::cout<<sub<<std::endl;
-   }
+  int t, n, k;
+  std::cin >> t;
+  while (t--)
+  {
+    std::cin >> n >> k;
+    int sub = n - k;
+    std::cout << sub << std::endl;
+  }
 
- return 0; 
+  return 0;
 }
