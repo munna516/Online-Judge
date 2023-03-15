1814c3853ccd7671092288a58f325d7623d1191f
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-03-15 14:35:06 +0600

    update

--- CODECHEF/PRESENTS.cpp
+++ CODECHEF/PRESENTS.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
- int test;
- std::cin>>test;
- while(test--)
-   {
+  int test;
+  std::cin >> test;
+  while (test--)
+  {
     long long int x;
-    cin>>x;
-    cout<<x-(x/5)<<endl;  
-   }
+    cin >> x;
+    cout << x - (x / 5) << endl;
+  }
 
- return 0; 
+  return 0;
 }
--- CODECHEF/WATERFLOW.cpp
+++ CODECHEF/WATERFLOW.cpp
@@ -1,21 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
- int test;
- std::cin>>test;
- while(test--)
+  int test;
+  std::cin >> test;
+  while (test--)
   {
-   int w,x,z,y;
-   std::cin>>w>>x>>y>>z;
-   if((z*y)+w>x)
-     std::cout<<"overFlow"<<std::endl;
-   else if((z*y)+w==x)
-     std::cout<<"filled"<<std::endl;  
-   else
-    std::cout<<"Unfilled"<<std::endl;
-
+    int w, x, z, y;
+    std::cin >> w >> x >> y >> z;
+    if ((z * y) + w > x)
+      std::cout << "overFlow" << std::endl;
+    else if ((z * y) + w == x)
+      std::cout << "filled" << std::endl;
+    else
+      std::cout << "Unfilled" << std::endl;
   }
-  
- return 0; 
+
+  return 0;
 }
