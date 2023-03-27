c3af5f832a65e700befb1fe8faf8db2c5a0d13a5
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-03-27 13:51:50 +0600

    update

--- CODECHEF/ACS.cpp
+++ CODECHEF/ACS.cpp
@@ -1,23 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
   int t;
-  cin>>t;
-  while(t--)
+  cin >> t;
+  while (t--)
+  {
+    int p;
+    cin >> p;
+    if (p >= 0 && p <= 10)
     {
-     int p;
-     cin>>p;
-     if(p>=0 && p<=10)
-       {
-        cout<<p<<endl;
-       }
-      else if((p/100)+(p%100)<=10)
-        {
-          cout<<(p/100)+(p%100)<<endl;
-        }
-      else
-        cout<<"-1"<<endl;    
+      cout << p << endl;
     }
- return 0;   
+    else if ((p / 100) + (p % 100) <= 10)
+    {
+      cout << (p / 100) + (p % 100) << endl;
+    }
+    else
+      cout << "-1" << endl;
+  }
+  return 0;
 }
--- CODECHEF/ADVANCE.cpp
+++ CODECHEF/ADVANCE.cpp
@@ -1,21 +1,21 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
- int test;
- std::cin>>test;
- while(test--)
-   {
-    int a,b;
-    std::cin>>a>>b;
-    if(b<a)
-      std::cout<<"NO"<<std::endl;
-    else if(b>=a && b<=a+200)
-      std::cout<<"YES"<<std::endl;
-    else 
-      std::cout<<"NO"<<std::endl;  
-   }
+  int test;
+  std::cin >> test;
+  while (test--)
+  {
+    int a, b;
+    std::cin >> a >> b;
+    if (b < a)
+      std::cout << "NO" << std::endl;
+    else if (b >= a && b <= a + 200)
+      std::cout << "YES" << std::endl;
+    else
+      std::cout << "NO" << std::endl;
+  }
 
- return 0; 
+  return 0;
 }
