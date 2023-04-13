d95d83ce07e64f717145c86632014ead093488ad
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-04-13 21:38:28 +0600

    update

--- CODECHEF/TEST.cpp
+++ CODECHEF/TEST.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int a;
-    do // We use a do-While and not just a while loop because the input command is inside the loop.
+    do
     {
         cin >> a;
-        if (a != 42 && a < 100 && a > (-100)) // Prints number only if it is not equal to 42 and not more than two digits
+        if (a != 42 && a < 100 && a > (-100))
             cout << a << "\n";
-    } while (a != 42 && a < 100 && a > (-100)); // The loop will continue only if the number is not equal to 42 and not more than two digits
+    } while (a != 42 && a < 100 && a > (-100));
+    return 0;
 }
