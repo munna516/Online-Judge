1688050a9e685775bcfc3720cfc65cc7e5329da3
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-04-26 12:33:22 +0600

    update

--- UVA/11219.c
+++ UVA/11219.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 int main()
 {
-    int a, b, c, d, x, y, age, t, i;
-    while (scanf("%d", &t) == 1)
+    int t, i, d, m, y, d1, m1, y1, age;
+    scanf("%d", &t);
+    for (i = 1; i <= t; i++)
     {
-        for (i = 1; i <= t; i++)
+
+        scanf("%d/%d/%d", &d, &m, &y);
+        getchar();
+        scanf("%d/%d/%d", &d1, &m1, &y1);
+        age = y - y1;
+        if (m1 > m || (m1 == m && d1 > d))
+            age--;
+        if (age < 0)
         {
-            scanf("%d/%d/%d", &b, &d, &y);
-            scanf("%d/%d/%d", &a, &c, &x);
-            if (a > b)
-            {
-                b = b + 30;
-                c = c + 1;
-            }
-            if (c > d)
-            {
-                d = d + 12;
-                x = x + 1;
-            }
-            age = y - x;
-            if (age < 0)
-                printf("Case #%d: Invalid birth date\n", i);
-            else if (age > 130)
-                printf("Case #%d: Check birth date\n", i);
-            else
-                printf("Case #%d: %d\n", i, age);
+
+            printf("Case #%d: Invalid birth date\n", i);
+        }
+        else if (age > 130)
+        {
+            printf("Case #%d: Check birth date\n", i);
+        }
+        else
+        {
+            printf("Case #%d: %d\n", i, age);
         }
     }
+
     return 0;
 }
