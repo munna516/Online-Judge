4af7f717cf5a618a0d75c20605b26c292ea33565
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-05-22 23:58:44 +0600

    Update

---
+++ Codeforces/1373B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ISO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define pi 3.1416
+#define endl "\n"
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+/*----------------------------------*/
+
+void solve()
+{
+    int count = 0;
+    getchar();
+    string s;
+    cin >> s;
+    int x = s.length() / 2;
+    if (x % 2 == 0)
+        cout << "NET" << endl;
+    else
+        cout << "DA" << endl;
+}
+int main()
+{
+   // ISO;
+    ll _;
+    cin >> _;
+    while (_--)
+    {
+        solve();
+    }
+    return 0;
+}
